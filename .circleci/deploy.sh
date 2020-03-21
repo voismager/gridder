eeb1bf3a0960b4476ca9e080607128144efa9303
gRastaSsS <dead.gerasim@gmail.com>
2020-03-21 18:51:30 +0300

    Update deploy.sh

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,1 +1,3 @@
 scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ~/repo/target/grid-0.0.1-SNAPSHOT.jar vlad@134.209.182.89:/home/vlad
+ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null vlad@134.209.182.89 systemctl --user daemon-reload
+ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null vlad@134.209.182.89 systemctl --user restart gridapp
--- src/main/java/com/example/grid/data/payload/AdvanceSearchRequest.java
+++ src/main/java/com/example/grid/data/payload/AdvanceSearchRequest.java
@@ -3,7 +3,10 @@ package com.example.grid.data.payload;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
+
 @Getter @Setter
 public class AdvanceSearchRequest extends DefaultPageRequest {
+    @NotNull
     private String query;
 }
