1e62ad3ecf7fde406a422b287830db2b3db76626
gRastaSsS <dead.gerasim@gmail.com>
2020-03-21 21:54:20 +0300

    Update Authenticated.java

--- src/main/java/com/example/grid/data/payload/user/Authenticated.java
+++ src/main/java/com/example/grid/data/payload/user/Authenticated.java
@@ -4,8 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
+
 @Getter @Setter
 @AllArgsConstructor
 public class Authenticated {
+    @NotNull
     private String accessToken;
 }
